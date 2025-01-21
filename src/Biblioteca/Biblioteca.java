821c325414c3bb45892ae38136b9b46fc7d66d87
ignacio-bc <ignaciobasilecairo@gmail.com>
2025-01-21 12:00:11 +0100

    v1.003

--- src/Biblioteca/Biblioteca.java
+++ src/Biblioteca/Biblioteca.java
@@ -13,12 +13,10 @@ import java.util.Scanner;
 public class Biblioteca {
 
     //Un atributo privado N que nos índique el número total de libros (poner por ejemplo 10).
-    private int N=10;
+    private int N = 10;
 //Un atributo libro de tipo Libro (que debe ser un array para guardar los 10 libros)
     public Libro[] libros = new Libro[N];
 //Un atributo privado numLibros para guardar el número de libros.
-    private int numLibros;
-    
 
     public Biblioteca() {
 //* Este método obligatoriamente debe ir generando todos los libros guardándolos en el array.
@@ -30,63 +28,105 @@ public class Biblioteca {
     }
 
     public Biblioteca(Libro[] libros, int cont) {
-       // Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 //* Dado un libro de tipo Libro [] introducir los libros y un número de libros que viene dado por el parámetro CONT.
-    
+
     }
 
     public Libro[] getLibros() {
-    return this.libros;
+        return this.libros;
     }
 
     public void setLibros(Libro[] libros) {
-    this.libros = libros;
+        this.libros = libros;
     }
 
     public int getCont() {
-    //* RETORNA EL NUMERO DE LIBROS;
+        return this.numLibros;
     }
 
     public void setCont(int cont) {
-//* Introduce el número de libros que viene dado por el parámetro CONT
+        this.numLibros = cont; //num de libros?
     }
-
-    public static int getN() {
-//* RETORNAR EL número N que libros que tenemos (en este caso 10);
-    }
+//
+//    public static int getN() {
+//        return this.N;
+//    }
 
     public boolean equals(Biblioteca biblioteca) {
 //* Devuelve TRUE si los libros de la biblioteca no son los mismos y FALSE en caso contrario. Tener en cuenta que tenemos GUARDADO LOS LIBROS en un array de libros.
+        for (int i = 0; i < this.libros.length; i++) {
+            if (!(this.libros[i].equals(biblioteca.libros[i]))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String toString() {
 //* Devuelve un STRING donde se va guardando todos los mensajes que tiene cada posición del array libros.
-        ST = ST + LIBROS[I].toString()
-        siendo ST una variable de tipo String
+//        ST = ST + LIBROS[I].toString()
+//        siendo ST una variable de tipo String 
+        String a = "";
+        a = a + "TAMAÑO DE LA BIBLIOTECA" + N + " LIBROS:\n+"
+                + "";
+        for (int i = 0; i < this.libros.length; i++) {
+            a = a + this.libros[i].toString() + "\n+";
+
+        }
+        return a;
     }
 
     public boolean buscar(long isbn) {
         /*Devuelve true si el libro está en la biblioteca
 Devuelve false si el libro no está en la biblioteca. Tener en cuenta los libros están en un array */
+        for (int i = 0; i < this.libros.length; i++) {
+            if (isbn == this.libros[i].getIsbn()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private int buscarPos(long isbn) {
 //* Dado un ISBN de un libro, devuelve la posición donde está dentro del array y si no está devuelve un -1.
+        int n = -1;
+        for (int i = 0; i < this.libros.length; i++) {
+            if (isbn == this.libros[i].getIsbn()) {
+                n = i;
+                return n;
+            }
+        }
+        return n;
+
     }
 
     public boolean insertar(Libro libro) {
         /*Si tengo espacio en la biblioteca y no hay otro libro
 con ese mismo código isbn SE INSERTA el libro que viene dado por el parámetro libro. Se aconseja usar el método anterior BUSCARPOS */
+        if (!(buscar(libro.getIsbn())));{
+            for (int i = 0; i < this.libros.length; i++) {
+                if (null == this.libros[i]) {
+                    this.libros[i] = libro;
+                    return true;
+                }
+            }
+        }
+        return false;
+
     }
 
     public boolean eliminar(long isbn) {
         /*Si existe un libro con ese isbn en la biblioteca lo borro
 en caso contrario no hago nada. Se aconseja usar el método BUSCARPOS*/
-    }
-    
-    public void prueba(){
-        for (int i = 0; i < libros.length; i++) {
-            System.out.println(libros[i]);
-        }
+        int a = this.buscarPos(isbn);
+       if(a>=0){//PREGUNTAR A IVAN
+           this.libros[a] = null;
+           System.out.println("Se elimino");
+           return true;
+       }
+       
+        System.out.println("No se elimino");
+        return false;
     }
 }
