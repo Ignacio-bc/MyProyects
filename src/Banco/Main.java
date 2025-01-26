ecbd0473ff2e519554ba3c6a9b8e038a34a4a521
ignacio-bc <ignaciobasilecairo@gmail.com>
2025-01-26 14:30:56 +0100

    v1.003

---
+++ src/Banco/Main.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Banco;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ignac
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Banco Banco = new Banco();
+        Scanner sc = new Scanner(System.in);
+        menuPrincipal(Banco);
+
+    }
+
+    public static void menuPrincipal(Banco Banco) {
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("1. Menu banco");
+            System.out.println("2. Menu Cliente");
+            System.out.println("3. Salir");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Menu Banco");
+                    menuBanco(Banco);
+                    break;
+                case 2:
+                    System.out.println("Menu Cliente");
+                    menuCliente(Banco.SeleccionarCliente());
+                    break;
+                case 3:
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+        } while (option != 3);
+    }
+
+    public static void menuCliente(Persona Cliente) {
+        Scanner sc = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("1. Sacar dinero");
+            System.out.println("2. Meter dinero");
+            System.out.println("3. Listar info del cliente");
+            System.out.println("4. Realizar Transferencia");
+            System.out.println("5. Registar Movimiento");
+            System.out.println("6. Volver al Menu");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Sacar dinero:");
+                    System.out.println("Cuanto dinero deseas retirar?");
+                    double retirar = sc.nextDouble();
+                    sc.nextLine();
+                    Cliente.seleccionarCuenta().retirarDinero(retirar);
+                    break;
+                case 2:
+                    System.out.println("Meter dinero");
+                    System.out.println("Cuantro desea ingresar?");
+                    double ingreso = sc.nextDouble();
+                    Cliente.seleccionarCuenta().ingresarSaldo(ingreso);
+                    break;
+                case 3:
+                    System.out.println("Listar info del cliente");
+                    break;
+                case 4:
+                    System.out.println("Realizar transferencia");
+                    break;
+                case 5:
+                    System.out.println("Registrar movimientos");
+                    break;
+                case 6:
+                    System.out.println("Volviendo al menu");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+        } while (option != 6);
+    }
+
+    public static void menuBanco(Banco Banco) {
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("1. Dar de alta cliente");
+            System.out.println("2. Dar de baja cliente");
+            System.out.println("3. Listar Cliente");
+            System.out.println("4. Cambio de titular");
+            System.out.println("5. Abrir una cuenta nueva");
+            System.out.println("6. Volver al menu Principal ");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Dar de alta cliente:");
+
+                    break;
+                case 2:
+                    System.out.println("Dar de baja cliente:");
+                    break;
+                case 3:
+                    System.out.println("Listar cliente:");
+                    break;
+                case 4:
+                    System.out.println("Cambio de titular:");
+                    break;
+                case 5:
+                    System.out.println("Abrir una cuenta nueva:");
+                    //Cuenta a = Banco.crearCuenta();
+                   // System.out.println("Seleccionar El mismo cliente para confirmar");
+                    Banco.altaCliente();
+                    Banco.verBanco();
+                    break;
+                case 6:
+                    System.out.println("Volviendo al menu principal");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+        } while (option != 6);
+    }
+}
